c709ac3b296be7628a59881a3783287783761805
Kevin Mulvey <kmulvey@shutterstock.com>
2013-09-03 15:01:05 -0400

    use mtime rather than git

--- tasks/bustr.js
+++ tasks/bustr.js
@@ -10,39 +10,28 @@ module.exports = function (grunt) {
 		var task = this.data.tasks;
 		var options = this.options();
 		var output = options.output;
-		var ext = options.ext;
 		VERSION = options.version;
 
-		getChangedFiles(src, output, ext);
+		statFile(src);
 	});
 	
-	function getChangedFiles(source_dir, output_dir, ext){
-    getBranch(function(branch){
-			var diff = spawn('git', ['diff','--name-only', 'master...'+branch, source_dir]);
-			diff.stdout.setEncoding('utf8');
-			diff.stdout.on('data', function (data) {
-				var files = data.split( "\n" ).reverse().filter(function( file ) {
-					if(file === ''){ return false;}
-					updateVersion(path.basename(file.trim(), '.'+ext),ext);
-				});
-			});
-			diff.stderr.on('data', function (data) {
-				console.log('ERROR GENERATING DIFF: ' + data);
-			});
-		});	
-	}
-	function getBranch(cb){
-    var branch = spawn('git', ['symbolic-ref', 'HEAD']);
-		branch.stdout.setEncoding('utf8');
-		branch.stdout.on('data', function (data) {
-			cb(data.replace('refs/heads/', '').trim());
+	// recursivly search the directory for files and return the mtime for each
+	function statFiles(path){
+		fs.readdirSync(path, function(err, files){
+			for(var i=0; i<files.length; i++){
+				if(fs.statSync(path+files[i]).isDirectory()){
+					statFiles(path+files[i]+'/');
+				}
+				updateVersion(path+files[i], fs.statSync(path+files[i]).mtime);
+			}
 		});
 	}
-	function updateVersion(file, ext){
+	
+	function updateVersion(file, time){
 		if(file === ''){ return;}
 		var data= fs.readFileSync(VERSION, 'utf8');
 		data = JSON.parse(data);
-		data[ext][file] = Math.round((new Date()).getTime() / 1000);
+		data[path.extname(file).replace('.','')][file] = Math.round((new Date()).getTime() / 1000);
 		fs.writeFileSync(VERSION, JSON.stringify(data), 'utf8');
 	}
 };
