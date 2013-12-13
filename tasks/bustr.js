b0b321a4e08636675c874504a1ce7ed0bdae4928
Kevin Mulvey <kmulvey@shutterstock.com>
2013-12-13 15:58:42 -0500

    use mtime and many fixes

--- tasks/bustr.js
+++ tasks/bustr.js
@@ -1,48 +1,33 @@
 'use strict';
 
-var path = require('path'), fs = require('fs'), spawn = require('child_process').spawn;
+var path = require('path'), fs = require('fs');
 
-var VERSION = null;
 module.exports = function (grunt) {
 	grunt.registerMultiTask('bustr', 'Bust yo chache', function () {
 		var done = this.async();
 		var src = this.data.src;
-		var task = this.data.tasks;
 		var options = this.options();
-		var output = options.output;
-		var ext = options.ext;
-		VERSION = options.version;
-
-		getChangedFiles(src, output, ext);
+		var version = options.version;
+		
+		grunt.file.recurse(src, function(abspath, rootdir, subdir, filename) {
+			statFile(abspath, version);
+		});
 	});
 	
-	function getChangedFiles(source_dir, output_dir, ext){
-    getBranch(function(branch){
-			var diff = spawn('git', ['diff','--name-only', 'master...'+branch, source_dir]);
-			diff.stdout.setEncoding('utf8');
-			diff.stdout.on('data', function (data) {
-				var files = data.split( "\n" ).reverse().filter(function( file ) {
-					if(file === ''){ return false;}
-					updateVersion(path.basename(file.trim(), '.'+ext),ext);
-				});
-			});
-			diff.stderr.on('data', function (data) {
-				console.log('ERROR GENERATING DIFF: ' + data);
-			});
-		});	
+	// recursivly search the directory for files and return the mtime for each
+	function statFile(path, version){
+		var dt = new Date(fs.statSync(path).mtime);
+		console.log(path + ' ... ' + fs.statSync(path).mtime);
+		updateVersion(path, dt.getTime(), version);
 	}
-	function getBranch(cb){
-    var branch = spawn('git', ['symbolic-ref', 'HEAD']);
-		branch.stdout.setEncoding('utf8');
-		branch.stdout.on('data', function (data) {
-			cb(data.replace('refs/heads/', '').trim());
-		});
-	}
-	function updateVersion(file, ext){
-		if(file === ''){ return;}
-		var data= fs.readFileSync(VERSION, 'utf8');
+	
+	function updateVersion(file, time, version){
+		var data = fs.readFileSync(version, 'utf8');
+		if(data === '') data = '{}'; // default it in case its a 0b file 
 		data = JSON.parse(data);
-		data[ext][file] = Math.round((new Date()).getTime() / 1000);
-		fs.writeFileSync(VERSION, JSON.stringify(data), 'utf8');
+		
+		if(data[path.extname(file).replace('.','')] === undefined) data[path.extname(file).replace('.','')] = {};
+		data[path.extname(file).replace('.','')][path.basename(file)] = time;
+		fs.writeFileSync(version, JSON.stringify(data), 'utf8');
 	}
 };
