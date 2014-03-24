97d0c4a8fc84cb4afbf74679cdd961d99d4d56b8
Kevin Mulvey <kmulvey@shutterstock.com>
2014-03-24 18:20:50 -0400

    spelling

--- tasks/bustr.js
+++ tasks/bustr.js
@@ -24,7 +24,7 @@ module.exports = function (grunt) {
 		});
 	});
 	
-	// recursivly search the directory for files and return the mtime for each
+	// recursively search the directory for files and return the mtime for each
 	function statFile(path, version_file){
 		var dt = new Date(fs.statSync(path).mtime);
 		var version_str = "" + dt.getFullYear() + dt.getMonth() + dt.getDate() + dt.getHours() + dt.getMinutes() + dt.getSeconds();
